3a12326d1c8f21272af2556ed34e89a8736a7d1d
trena <20079502+tholst952@users.noreply.github.com>
2023-09-28 14:33:03 -0700

    fix computerSelection showing up as undefined in game messages

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 'use strict';
 
-const getComputerChoice = function () {
+// this functions generates a random choice for the computer
+const comp = function getComputerChoice() {
     const options = ['rock', 'paper', 'scissors'];
     const x = Math.floor(Math.random() * 3);
-    console.log(options[x])
+    console.log(options[x]);
 }
 
-getComputerChoice()
+let playerChoice = prompt('What do you choose? rock, paper or scissors?').toLowerCase();
+
+// this function plays a single round of the game
+function playGame (playerSelection, computerSelection) {
+    if(playerSelection === computerSelection) {
+        console.log(`Draw. No win. 🤷🏻‍♀️`);
+    } else if (playerSelection === 'paper' && computerSelection === 'rock' ||
+    playerSelection === 'scissors' && computerSelection === 'paper' ||
+    playerSelection === 'rock' && computerSelection === 'scissors') {
+        console.log(`You win! ${playerSelection} beats ${computerSelection} 🥳`);
+    } else {
+        console.log(`You lose! ${computerSelection} beats ${playerSelection} ❌`);
+    }
+    
+}
+
+playGame(playerChoice, comp());
