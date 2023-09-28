7b5a58218aab5ca26d49aeff603da8d1f6fb50ed
trena <20079502+tholst952@users.noreply.github.com>
2023-09-28 14:36:13 -0700

    playGame function working

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 const comp = function getComputerChoice() {
     const options = ['rock', 'paper', 'scissors'];
     const x = Math.floor(Math.random() * 3);
-    console.log(options[x]);
+    return options[x];
 }
 
 let playerChoice = prompt('What do you choose? rock, paper or scissors?').toLowerCase();
