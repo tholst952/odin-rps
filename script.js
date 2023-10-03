721e690ab095c2c2419cfe9303cb571c89587e45
trena <20079502+tholst952@users.noreply.github.com>
2023-10-03 10:37:58 -0700

    added comments and 5 full rounds. Still struggling with using a loop to invoke game function 5 times

--- script.js
+++ script.js
@@ -28,39 +28,46 @@ function playRound(playerSelection, computerSelection) {
     return `You lose! ${computerSelection} beats ${playerSelection} ❌`;
   }
 }
+
+console.log(playRound(playerChoice(), comp()));
+console.log(playRound(playerChoice(), comp()));
+console.log(playRound(playerChoice(), comp()));
 */
-// console.log(playRound(playerChoice(), comp()));
-// console.log(playRound(playerChoice(), comp()));
-// console.log(playRound(playerChoice(), comp()));
 
 function game() {
+  // Initial score counters
   let playerScore = 0;
   let compScore = 0;
 
-  function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
+  // Play a round of the game
+  function playRound(player, comp) {
+    if (player === comp) {
       console.log(`Draw. No win. 🤷🏻‍♀️`);
-    } else if (playerSelection === "") {
+    } else if (player === "") {
       console.log(`Please choose a play: rock, paper or scissors`);
     } else if (
-      (playerSelection === "paper" && computerSelection === "rock") ||
-      (playerSelection === "scissors" && computerSelection === "paper") ||
-      (playerSelection === "rock" && computerSelection === "scissors")
+      (player === "paper" && comp === "rock") ||
+      (player === "scissors" && comp === "paper") ||
+      (player === "rock" && comp === "scissors")
     ) {
       playerScore++;
       console.log(
-        `You win! ${playerSelection} beats ${computerSelection} 🥳 Your current Score: ${playerScore}`
+        `You win! ${player} beats ${comp} 🥳 Your current Score: ${playerScore}`
       );
     } else {
       compScore++;
-      console.log(`You lose! ${computerSelection} beats ${playerSelection} ❌`);
+      console.log(`You lose! ${comp} beats ${player} ❌`);
     }
   }
 
+  // Play 5 rounds of the game
+  playRound(playerChoice(), comp());
+  playRound(playerChoice(), comp());
   playRound(playerChoice(), comp());
   playRound(playerChoice(), comp());
   playRound(playerChoice(), comp());
 
+  // Final scores output message
   console.log(`Final Scores--- You: ${playerScore} Computer: ${compScore}`);
   if (playerScore === compScore) {
     console.log(`It seems that we have a tie. ＼（〇_ｏ）／`);
@@ -71,4 +78,50 @@ function game() {
   }
 }
 
+// Execute the function
 game();
+
+////////////////////////////////////////////////////////////
+// Tried using a loop, couldn't get it to work right..
+
+// function game() {
+//   let playerScore = 0;
+//   let compScore = 0;
+
+//   function playRound(player, comp) {
+//     for (let i = 0; i <= 5; i++) {
+//       if (player === comp) {
+//         console.log(`Draw. No win. 🤷🏻‍♀️`);
+//       } else if (player === "") {
+//         console.log(`Please choose a play: rock, paper or scissors`);
+//       } else if (
+//         (player === "paper" && comp === "rock") ||
+//         (player === "scissors" && comp === "paper") ||
+//         (player === "rock" && comp === "scissors")
+//       ) {
+//         playerScore++;
+//         console.log(
+//           `You win! ${player} beats ${comp} 🥳 Your current Score: ${playerScore}`
+//         );
+//       } else {
+//         compScore++;
+//         console.log(`You lose! ${comp} beats ${player} ❌`);
+//       }
+
+//       if (i === 5) {
+//         console.log(
+//           `Final Scores--- You: ${playerScore} Computer: ${compScore}`
+//         );
+//         if (playerScore === compScore) {
+//           console.log(`It seems that we have a tie. ＼（〇_ｏ）／`);
+//         } else if (playerScore > compScore) {
+//           console.log(`Congrats, human! 🌟🤩🌾😊😎🌾🍀`);
+//         } else {
+//           console.log(`The computer is victorious. o((⊙﹏⊙))o.`);
+//         }
+//       }
+//     }
+//   }
+// }
+
+// game();
