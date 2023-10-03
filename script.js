72547e9cc6b11298265c9d3b1eecd9c900eaf8cf
trena <20079502+tholst952@users.noreply.github.com>
2023-10-03 08:38:01 -0700

    turned playerChoice into a function so I can call it multiple times and have a different game each call

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
-'use strict';
+"use strict";
 
 // this functions generates a random choice for the computer
 const comp = function getComputerChoice() {
-    const options = ['rock', 'paper', 'scissors'];
-    const x = Math.floor(Math.random() * 3);
-    return options[x];
-}
+  const options = ["rock", "paper", "scissors"];
+  const x = Math.floor(Math.random() * 3);
+  return options[x];
+};
 
-let playerChoice = prompt('What do you choose? rock, paper or scissors?').toLowerCase();
+// this function get an input choice from the user via prompt
+const playerChoice = function () {
+  return prompt("What do you choose? rock, paper or scissors?").toLowerCase();
+};
 
 // this function plays a single round of the game
-function playGame (playerSelection, computerSelection) {
-    if(playerSelection === computerSelection) {
-        console.log(`Draw. No win. 🤷🏻‍♀️`);
-    } else if (playerSelection === 'paper' && computerSelection === 'rock' ||
-    playerSelection === 'scissors' && computerSelection === 'paper' ||
-    playerSelection === 'rock' && computerSelection === 'scissors') {
-        console.log(`You win! ${playerSelection} beats ${computerSelection} 🥳`);
-    } else {
-        console.log(`You lose! ${computerSelection} beats ${playerSelection} ❌`);
-    }
-    
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return `Draw. No win. 🤷🏻‍♀️`;
+  } else if (
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "rock" && computerSelection === "scissors")
+  ) {
+    return `You win! ${playerSelection} beats ${computerSelection} 🥳`;
+  } else {
+    return `You lose! ${computerSelection} beats ${playerSelection} ❌`;
+  }
 }
 
-playGame(playerChoice, comp());
+console.log(playRound(playerChoice(), comp()));
+console.log(playRound(playerChoice(), comp()));
+console.log(playRound(playerChoice(), comp()));
